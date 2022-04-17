8dc3dbd240179df605ed4cad4c2f86b52436ed5d
Sunrise-1337 <aozoraishere@gmail.com>
2022-04-17 15:04:10 +0300

    ДЗ

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
+<<<<<<< Updated upstream
 var userName = prompt('What is your name?');
 alert('Hello, ' + userName + '! How are you?');
+=======
+var m = '#',
+    n = '\n#';
+
+for( var i = 0; i < 7; i++){
+  n = n + '#',
+  m = m + n;
+}
+
+console.log(m)
+
+var line = "############################\n",
+    rest = "#####                 ######\n##   ***                **##\n#   *##**         **  O  *##\n#    ***     O    ##**    *#\n#       O         ##***    #\n#                 ##**     #\n#   O       #*             #\n#*          #**       O    #\n#***        ##**    O    **#\n##****     ###***       *###\n";
+var c = 0,
+    mes = '',
+    d = 0;
+
+while ( c < 3) {
+  c++;
+  mes = mes + line;
+  while (d  < 1) {
+    mes = mes + rest;
+    d++;
+  }
+  if (c == 2){
+    console.log(mes);
+  }
+}
+>>>>>>> Stashed changes
