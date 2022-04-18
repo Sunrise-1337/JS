9a50f0fb61a2614674117f0935594685e5ed1438
Sunrise-1337 <aozoraishere@gmail.com>
2022-04-18 22:35:56 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,2 +1,51 @@
+<<<<<<< Updated upstream
 var userName = prompt('What is your name?');
 alert('Hello, ' + userName + '! How are you?');
+=======
+//  1 задача
+var act = '';
+
+for (;;){
+    act = prompt('What to do?');
+    if (act == '+'||act == '-'||act == '/'||act == '*' ) break;
+}
+console.log(act);
+
+//  2 задача
+var am = 0;
+
+for (;;){
+    am = prompt('How many numbers?') - 1;
+    if (am > 1) break;
+}
+console.log(am);
+
+// 3 задача
+var result = 0;
+
+for (;;){
+    result = +prompt('First number?');
+    if (typeof(result) == 'number') break;
+}
+console.log(am);
+
+for (var i = 0; i < am; i++){
+    var keep = +prompt('Next number?');
+    if (!keep) {
+        alert('Error');
+        break;
+    }
+    switch (act){
+        case '+': result = result + keep;
+            break;
+        case '-': result = result - keep;
+            break;
+        case '/': result = result / keep;
+            break;
+        case '*': result = result * keep;
+    }
+}
+
+// 4 задача
+alert(result);
+>>>>>>> Stashed changes
