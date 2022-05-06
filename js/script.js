24d3f0126407432136b30c030c44c5b26d264e13
Sunrise-1337 <aozoraishere@gmail.com>
2022-05-06 15:09:09 +0300

    UPD

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 var userName = prompt('What is your name?');
-alert('Hello, ' + userName + '! How are you?');
+alert('Hello, ' + userName + '! How are you?');// Задача 1
+
+// var array = [2, 3, 4, 5, 6, 7],
+//     l = array.length,
+//     result = 0;
+
+// function toSum(arr, arrL){
+//     arrL = arrL - 1;
+//     if (arrL >= 0) {
+//         result = arr[arrL] + toSum(arr, arrL)
+//     }
+//     return result
+// }
+
+// toSum(array, l)
+
+// console.log(result)
+
+// Задача 2
+
+// var calc = 0;
+
+// function retSum(start, finish, step){
+//     if (start < 0 || finish < 0 || step <= 0){
+//         return 0
+//     }
+
+//     calc += start;
+
+//     if ((start + step) <= finish) {
+//         start = (start + step) + retSum(start+step, finish, step)
+//     }
+// }
+
+// retSum(0, 10, 3)
+
+// console.log(calc)
