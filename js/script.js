32bc7ee3f11632e05d17625811e501f6486333fd
Sunrise-1337 <aozoraishere@gmail.com>
2022-05-10 16:27:57 +0300

    HW-8

--- js/script.js
+++ js/script.js
@@ -1,2 +1,37 @@
 var userName = prompt('What is your name?');
 alert('Hello, ' + userName + '! How are you?');
+function makePassword(){
+    // получаем пароль с сервера
+    var password = 'Vladislav2003';
+
+    return function checkPassword(ans){
+        return (ans == password)
+    }
+}
+
+var pass1 = prompt('Password?'),
+    pro = makePassword();
+
+pro(pass1);
+console.log(pro(pass1))
+
+// Задача 2
+
+function randomGacha(){
+    var store = [];
+
+    return function randNum(){
+        var rand = Math.floor(Math.random()*100);
+        if (!store.includes(rand)){
+            store.push(rand);
+            console.log(store)
+        }
+    }
+}
+
+var test = randomGacha();
+
+for (var i = 0; i < 60; i++){
+    test();    
+}
+>>>>>>> Stashed changes
