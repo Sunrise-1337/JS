cfc7a034ba24721246894b4506cadfe8e1f3a56f
Sunrise-1337 <aozoraishere@gmail.com>
2022-05-12 21:58:52 +0300

    HW-9

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-var userName = prompt('What is your name?');
-alert('Hello, ' + userName + '! How are you?');
+// Задача 1
+
+var tSet = prompt('Введите время в формате чч:мм');
+var time = tSet.split(':');
+
+var clock = setInterval(function (){
+    var trash = new Date(),
+    tSet2 = trash.toLocaleTimeString(),
+    time2 = tSet2.split(':');
+
+    document.getElementById('clock').innerHTML = tSet2;
+
+    if (time[0] == time2[0] && time[1] == time2[1]){
+        alert('Wake up, Samurai. We have a city to burn')
+        return clearInterval(clock)
+    }
+}, 1000);
