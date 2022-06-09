29ef6ffbc2488b5c7ec73dc908b689038f3649b5
Sunrise-1337 <aozoraishere@gmail.com>
2022-06-09 13:22:57 +0300

    UPD 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
+const worKer = {
+    name: '',
+    surname: '',
+    id: '',
+    rate: '',
+    days: '',
+    getSalary: () => {},
+    role: 'роль',
+    currentCash: '',
+}
 
+admin(cashier){
+    photo: '',
+    triplePremium: '',
+}
+
+cashier(worKer){
+    premium: '',
+    getSalary: (id) => {
+        return worKer.rate * worKer.days * 1.2
+    },
+}
+
+consult(worKer){
+    expireDate: '',
+    getSalary: (id) => {
+        return worKer.rate * worKer.days
+    }
+}
+
+const empl = () => {
+    let employee = 3
+
+    return {
+        add: () => employee += 1,
+        remove : () => employee += 1,
+        display: () => console.log(employee),
+    }
+}
+
+let user = empl();
