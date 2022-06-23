15152ba2d82ed576ab89c73b9d26779ca23b21d9
Sunrise-1337 <aozoraishere@gmail.com>
2022-06-23 19:09:52 +0300

    UPD

--- js/script.js
+++ js/script.js
@@ -1,1 +1,104 @@
+const generate = () => {
+    let wrap = document.createElement('div'),
+        enHead = document.createElement('h2'),
+        enemy = document.createElement('div'),
+        plHead = document.createElement('h2'),
+        playerWrap = document.createElement('div'),
+        scissors = document.createElement('img'),
+        rock = document.createElement('img'),
+        paper = document.createElement('img');
+    
+    wrap.className = 'wrap';
+    enHead.className = 'enHead';
+    enemy.className = 'enemy';
+    plHead.className = 'plHead';
+    playerWrap.className = 'playerWrap';
+    scissors.className = 'scissors pic';
+    rock.className = 'rock pic';
+    paper.className = 'paper pic';
 
+    rock.setAttribute('src', './assets/rock.png')
+    scissors.setAttribute('src', './assets/scissors.png')
+    paper.setAttribute('src', './assets/paper.png')
+
+    enHead.innerText = 'Enemy:'
+    plHead.innerText = 'Your choice?'
+
+    document.body.append(wrap);
+    wrap.append(enHead);
+    wrap.append(enemy);
+    wrap.append(plHead);
+    wrap.append(playerWrap);
+    playerWrap.append(rock);
+    playerWrap.append(scissors);
+    playerWrap.append(paper);
+}
+
+generate()
+
+let enemy = document.querySelector('.enemy'),
+    rockZ = document.querySelector('.rock'),
+    scissorsZ = document.querySelector('.scissors'),
+    paperZ = document.querySelector('.paper'),
+    choice = '',
+    enChoice = '';
+
+function click(opt){
+    choice = opt
+    enemyTurn()
+    check(opt)
+}
+
+function enemyTurn(){
+    let enChoi = Math.floor(Math.random() * (2 + 1))
+    switch(enChoi) {
+        case 0:
+            enChoice = 'rock'
+            break
+      
+        case 1:
+            enChoice = 'scissors'
+            break
+        case 2:
+            enChoice = 'paper'
+            break
+    }
+}
+
+function check(opt){
+    let enPic = document.querySelector('.enPic');
+
+    if (!enPic) {
+        let ePi = document.createElement('img');
+        ePi.className = 'enPic';
+        ePi.setAttribute('src', `./assets/${enChoice}.png`);
+        enemy.append(ePi);
+    } else {
+        enPic.setAttribute('src', `./assets/${enChoice}.png`);
+    }
+
+    if (opt == enChoice) return alert('Tie')
+    switch(opt) {
+        case `rock`:
+            enChoice == `paper` ? alert('You lost') : alert('You won')
+            break
+        case `scissors`:
+            enChoice == `rock` ? alert('You lost') : alert('You won')
+            break
+        case `paper`:
+            enChoice == `scissors` ? alert('You lost') : alert('You won')
+            break
+    }
+}
+
+rockZ.addEventListener('click', () =>{
+    click(`rock`)
+})
+
+scissorsZ.addEventListener('click', () =>{
+    click(`scissors`)
+})
+
+paperZ.addEventListener('click', () =>{
+    click(`paper`)
+})
